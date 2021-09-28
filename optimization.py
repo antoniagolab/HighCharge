49a566aaf9a478ec172dd1b72fa537cf99dd3c04
Antonia Golab <golab@golab-nb.eeg.tuwien.ac.at>
2021-09-28 15:28:24 +0200

    adding removal of parking places depending on charging time threshold and building infrastructure for rush hour

--- optimization.py
+++ optimization.py
@@ -6,12 +6,27 @@ Optimization of charging station allocation
 
 last edit: 2021/12/08
 
+
+TODO:
+    - if waiting time > 10min to charge, then remove parking places (3)
+    - recalculate demand based on this (4)
+    - make it possible to read status quo and set constraints based on these
+    - recalculate demand based on this
+    - evaluate whether charging stations are to be placed based on profitability
+    - cost minimum objective function
+    - some small e-charge which is to be removed? -> some small amount for which it is not profitable to be covered
+
+
+TODO: comparison of different optimization strategies: placing a charging station everywhere where it is profitable
+    based on the local demand - then (A) minimizing total costs OR (B) setting for each charging station a profitability
+    constraint but maximizing the amount of charging stations
 """
+
 
 import pandas as pd
 import numpy as np
-from pyomo.environ import *
 from optimization_parameters import *
+from pyomo.environ import *
 import time
 import warnings
 from pandas.core.common import SettingWithCopyWarning
@@ -35,14 +50,55 @@ def optimization(
 
     :return:
     """
+    # removing parking places from possible locations if charging time < 10 min
+    # and recalculate demands
+    asfinag_type_list_0 = dir_0[col_type].to_list()
+    asfinag_type_list_1 = dir_1[col_type].to_list()
+    dir_0_inds = dir_0.index.to_list()
+    dir_1_inds = dir_1.index.to_list()
+    if time_of_charging > max_charging_at_pp:
+            for name in ['A1']:
+                extract_dir_0 = dir_0[dir_0[col_highway] == name]
+                extract_dir_1 = dir_1[dir_1[col_highway] == name]
+                for ij in range(0, len(extract_dir_0)-1):
+                    if asfinag_type_list_0[ij] == 2:
+                        energy_demand_0[ij + 1] = energy_demand_0[ij] + energy_demand_0[ij+1]
+
+                for ij in range(0, len(extract_dir_1)-1):
+                    if asfinag_type_list_1[ij] == 2:
+                        energy_demand_1[ij + 1] = energy_demand_1[ij] + energy_demand_1[ij + 1]
+
+
+            refac_dir_0 = dir_0
+            refac_dir_1 = dir_1
+            refac_dir_0[col_energy_demand] = energy_demand_0
+            refac_dir_1[col_energy_demand] = energy_demand_1
+            refac_dir_0 = refac_dir_0[~(refac_dir_0[col_type] == 2)]
+            refac_dir_1 = refac_dir_1[~(refac_dir_1[col_type] == 2)]
+            refac_energy_demand_0 = [d * tf_at_peak for d in refac_dir_0[
+                col_energy_demand].to_list()]  # (kWh/d) energy demand at each rest area per day
+            refac_energy_demand_1 = [d * tf_at_peak for d in refac_dir_1[col_energy_demand].to_list()]
+    else:
+        refac_dir_0 = dir_0
+        refac_dir_1 = dir_1
+        refac_energy_demand_0 = [d * tf_at_peak for d in refac_dir_0[
+            col_energy_demand].to_list()]  # (kWh/d) energy demand at each rest area per day
+        refac_energy_demand_1 = [d * tf_at_peak for d in refac_dir_1[col_energy_demand].to_list()]
+
+    refac_dir_0["ID"] = range(0, len(refac_dir_0))
+    refac_dir_0 = refac_dir_0.set_index("ID")
+
+    refac_dir_1["ID"] = range(0, len(refac_dir_1))
+    refac_dir_1 = refac_dir_1.set_index("ID")
     model = ConcreteModel()  # Model initialization
 
+    n0 = len(refac_dir_0)
+    n1 = len(refac_dir_1)
     # --------------------------------------------- variable definition ---------------------------------------------
     # driving directions "normal" (=0) and "inverse" (=1) are treated separately throughout the optimization model
     # charging demand at each resting area is then summed up if it is for both directions
-
-    model.IDX_0 = range(n0)
-    model.IDX_1 = range(n1)
+    model.IDX_0 = range(0, n0)
+    model.IDX_1 = range(0, n1)
     model.IDX_2 = range(0, n3)
 
     # binary variables stating whether a charging station is installed at given ra (=resting area)
@@ -67,25 +123,25 @@ def optimization(
     model.pE_charged_0 = Var(model.IDX_0)
     model.pE_charged_1 = Var(model.IDX_1)
 
-    cars_per_day = hours_of_constant_charging / (acc / charging_capacity)
-    energy = acc * cars_per_day  # (kWh) charging energy per day by one charging pole
+    cars_per_hour = hourly_res / (acc / charging_capacity)
+    energy = acc * cars_per_hour  # (kWh) charging energy per day by one charging pole
     # ------------------------------------------------- constraints -------------------------------------------------
 
     # the pE_Zu needs to be 0 at each first ra as it is assumed that no energy demand is previously covered;
     # all energy demand is constrained to be covered at the last ra on a highway
     model.c1 = ConstraintList()
 
-    firsts_0 = dir_0[dir_0["first"] == True].index
+    firsts_0 = refac_dir_0[refac_dir_0["first"]].index
     for ind in firsts_0:
         model.c1.add(model.pE_input_0[ind] == 0)
-    lasts_0 = dir_0[dir_0["last"] == True].index
+    lasts_0 = refac_dir_0[refac_dir_0["last"]].index
     for ind in lasts_0:
         model.c1.add(model.pE_output_0[ind] == 0)
 
-    firsts_1 = dir_1[dir_1["first"] == True].index
+    firsts_1 = refac_dir_1[refac_dir_1["first"]].index
     for ind in firsts_1:
         model.c1.add(model.pE_input_1[ind] == 0)
-    lasts_1 = dir_1[dir_1["last"] == True].index
+    lasts_1 = refac_dir_1[refac_dir_1["last"]].index
     for ind in lasts_1:
         model.c1.add(model.pE_output_1[ind] == 0)
 
@@ -99,29 +155,29 @@ def optimization(
 
     # constraining the fictitiously passed on energy to ensure evenly distributed charging network
 
-    dir_0_highways = dir_0[col_highway].to_list()
+    dir_0_highways = refac_dir_0[col_highway].to_list()
     model.c3 = ConstraintList()
     for ij in model.IDX_0:
         highway = dir_0_highways[ij]
         model.c3.add(
-            model.pE_input_0[ij] / (traffic_flows_dir_0[ij] * specific_demand * eta)
+            model.pE_input_0[ij] / (traffic_flows_dir_0[ij] * specific_demand * tf_at_peak * eta * mu)
             <= maximum_dist_between_charging_stations
         )
         model.c3.add(
-            model.pE_output_0[ij] / (traffic_flows_dir_0[ij] * specific_demand * eta)
+            model.pE_output_0[ij] / (traffic_flows_dir_0[ij] * specific_demand * tf_at_peak * eta * mu)
             <= maximum_dist_between_charging_stations
         )
 
-    dir_1_highways = dir_1[col_highway].to_list()
+    dir_1_highways = refac_dir_1[col_highway].to_list()
     model.c4 = ConstraintList()
     for ij in model.IDX_1:
         highway = dir_1_highways[ij]
         model.c4.add(
-            model.pE_input_1[ij] / (traffic_flows_dir_1[ij] * specific_demand * eta)
+            model.pE_input_1[ij] / (traffic_flows_dir_1[ij] * specific_demand * tf_at_peak * eta * mu)
             <= maximum_dist_between_charging_stations
         )
         model.c4.add(
-            model.pE_output_1[ij] / (traffic_flows_dir_1[ij] * specific_demand * eta)
+            model.pE_output_1[ij] / (traffic_flows_dir_1[ij] * specific_demand * tf_at_peak * eta * mu)
             <= maximum_dist_between_charging_stations
         )
 
@@ -130,7 +186,7 @@ def optimization(
     for ij in model.IDX_0:
         model.c7.add(
             model.pE_charged_0[ij]
-            - energy_demand_0[ij] * eta
+            - refac_energy_demand_0[ij] * eta * mu
             - model.pE_input_0[ij]
             + model.pE_output_0[ij]
             == 0
@@ -140,7 +196,7 @@ def optimization(
     for ij in model.IDX_1:
         model.c8.add(
             model.pE_charged_1[ij]
-            - energy_demand_1[ij] * eta
+            - refac_energy_demand_1[ij] * eta * mu
             - model.pE_input_1[ij]
             + model.pE_output_1[ij]
             == 0
@@ -166,18 +222,18 @@ def optimization(
         name = dir_names[ij]
         direc = dir_directions[ij]
         highway = dir_highways[ij]
-        extract_dir_0 = dir_0[
+        extract_dir_0 = refac_dir_0[
             (
-                (dir_0[col_rest_area_name] == name)
-                & (dir_0[col_directions] == direc)
-                & (dir_0[col_highway] == highway)
+                (refac_dir_0[col_rest_area_name] == name)
+                & (refac_dir_0[col_directions] == direc)
+                & (refac_dir_0[col_highway] == highway)
             )
         ].index.to_list()
-        extract_dir_1 = dir_1[
+        extract_dir_1 = refac_dir_1[
             (
-                (dir_1[col_rest_area_name] == name)
-                & (dir_1[col_directions] == direc)
-                & (dir_1[col_highway] == highway)
+                (refac_dir_1[col_rest_area_name] == name)
+                & (refac_dir_1[col_directions] == direc)
+                & (refac_dir_1[col_highway] == highway)
             )
         ].index.to_list()
         if len(extract_dir_0) > 0 and len(extract_dir_1) > 0:
@@ -208,12 +264,13 @@ def optimization(
         model.c14.add(model.pE_output_1[ij] >= 0)
 
     # ------------------------------------------------- objective -------------------------------------------------
-    # maximization of revenue during the observed observation period
+    # maximization of profit during the observed observation period
+    # TODO: change to minimization of costs
 
     model.obj = Objective(
         expr=(
-            sum(model.pE_charged_0[n] * (ec - e_tax) for n in model.IDX_0)
-            + sum(model.pE_charged_1[n] * (ec - e_tax) for n in model.IDX_1)
+            sum(model.pE_charged_0[n] * (ec - e_tax) for n in model.IDX_0) * 24 * tf_at_peak
+            + sum(model.pE_charged_1[n] * (ec - e_tax) for n in model.IDX_1) * 24 * tf_at_peak
             - (
                 1
                 / RBF
@@ -293,8 +350,8 @@ def optimization(
         col_type,
         "pXi_dir",
     ]
-    output_dir_0 = dir_0[output_cols]
-    output_dir_1 = dir_1[output_cols]
+    output_dir_0 = refac_dir_0[output_cols]
+    output_dir_1 = refac_dir_1[output_cols]
     output_dir_0["pXi_dir"] = pXi_dir_0
     output_dir_1["pXi_dir"] = pXi_dir_1
     output_dir_0["pYi_dir_0"] = pYi_dir_0
@@ -311,8 +368,8 @@ def optimization(
     all_info_df.index = range(0, len(all_info_df))
     all_info_df = all_info_df.fillna(0.0)
     output_dataframe = pd.DataFrame()
-    dir_0_names = dir_0[col_rest_area_name].to_list()
-    dir_1_names = dir_1[col_rest_area_name].to_list()
+    dir_0_names = refac_dir_0[col_rest_area_name].to_list()
+    dir_1_names = refac_dir_1[col_rest_area_name].to_list()
     all_ra_names = list(set(dir_0_names + dir_1_names))
     output_dataframe.index = all_ra_names
     key_list = all_info_df.keys().to_list()
@@ -385,6 +442,10 @@ def optimization(
         + str(cvar1)
         + ";  eta="
         + str(eta)
+        + "; mu="
+        + str(mu)
+        + "; tf_at_peak="
+        + str(tf_at_peak)
         + ";  hours_of_constant_charging="
         + str(hours_of_constant_charging)
         + ";  i="
@@ -409,7 +470,7 @@ def optimization(
         ec=ec,
         acc=acc,
         charging_capacity=charging_capacity,
-        cars_per_day=cars_per_day,
+        cars_per_day=cars_per_hour,
         energy=energy,
         specific_demand=specific_demand,
     )
--- optimization_parameters.py
+++ optimization_parameters.py
@@ -9,6 +9,7 @@ import pandas as pd
 dir_0 = pd.read_csv(
     "data/rest_area_0_input_optimization_v3.csv"
 )  # file containing service stations for both directions + for "normal" direction
+
 dir_1 = pd.read_csv(
     "data/rest_area_1_input_optimization_v3.csv"
 )  # file containing service stations for both directions + for "inverse" direction
@@ -23,7 +24,6 @@ col_type = "asfinag_type"
 col_position = "asfinag_position"
 col_highway = "highway"
 
-
 dir = dir_0.append(dir_1)
 dir = dir[[col_highway, col_rest_area_name, col_position, col_directions]]
 dir = dir.sort_values(by=[col_highway, col_position])
@@ -35,7 +35,7 @@ n0 = len(dir_0)
 n1 = len(dir_1)
 k = len(dir)
 n3 = k
-# 50 kW annehmen -> + 20h durchgehende Besetzung
+# 50 kW annehmen
 g = 10000  # Maximum number of charging poles at one charging station
 specific_demand = 0.2  # (kWh/100km) average specific energy usage for 100km
 acc = (
@@ -47,27 +47,34 @@ e_tax = 0.15  # (€/kWh) total taxes and other charges
 cfix = 50000  # (€) total installation costs of charging station installation
 cvar1 = 10000  # (€) total installation costs of charging pole installation
 eta = 0.011  # share of electric vehicles of car fleet
+mu = 0.5    # share of vehicles which travel long-distance and might have to charge along highway
+tf_at_peak = 0.14    # share of accumulation at peak demand time (hourly res.) of all cars passed throughout the day
 hours_of_constant_charging = (
     20  # number of hours of continuous charging at one charging pole
 )
-energy_demand_0 = dir_0[
-    col_energy_demand
-].to_list()  # (kWh/d) energy demand at each rest area per day
-energy_demand_1 = dir_1[col_energy_demand].to_list()
+hourly_res = 1  # (h)
+energy_demand_0 = [d * tf_at_peak for d in dir_0[
+    col_energy_demand].to_list()]   # (kWh/d) energy demand at each rest area per day
+energy_demand_1 = [d * tf_at_peak for d in dir_1[col_energy_demand].to_list()]
 directions_0 = dir_0[col_directions].to_list()
 directions_1 = dir_1[col_directions].to_list()
 
-cars_per_day = hours_of_constant_charging / (acc / charging_capacity)
-energy = acc * cars_per_day  # (kWh) charging energy per day by one charging pole
+cars_per_hour = hourly_res / (acc / charging_capacity)
+
+time_of_charging = acc / charging_capacity
+max_charging_at_pp = 10/60  # (h) maximum time of charging at a parking place
+
+
+energy = acc * cars_per_hour  # (kWh) charging energy in resolution time t by one charging pole
 
 e_average = (
-    (sum(energy_demand_0) + sum(energy_demand_1)) * eta / (n0 + n1)
+    (sum(energy_demand_0) + sum(energy_demand_1)) * tf_at_peak * mu * eta / (n0 + n1)
 )  # (kWh/d) average energy demand at a charging station per day
 i = 0.05  # interest rate
 T = 10  # (a) calculation period für RBF (=annuity value)
 RBF = 1 / i - 1 / (i * (1 + i) ** T)  # (€/a) annuity value for period T
 
-maximum_dist_between_charging_stations = 30  # (km)
+maximum_dist_between_charging_stations = 100  # (km)
 traffic_flows_dir_0 = dir_0[col_traffic_flow].to_list()
 traffic_flows_dir_1 = dir_1[col_traffic_flow].to_list()
 
@@ -100,12 +107,19 @@ e_average_1 = {}
 for name in highway_names:
     energy_demand_dir_0 = dir_0[dir_0[col_highway] == name][col_energy_demand].to_list()
     if len(energy_demand_dir_0) > 0:
-        e_average_0[name] = np.average(energy_demand_dir_0) * eta
+        e_average_0[name] = np.average(energy_demand_dir_0) * tf_at_peak * eta * mu
     else:
         e_average_0[name] = 0
 
     energy_demand_dir_1 = dir_1[dir_1[col_highway] == name][col_energy_demand].to_list()
     if len(energy_demand_dir_1) > 0:
-        e_average_1[name] = np.average(energy_demand_dir_1) * eta
+        e_average_1[name] = np.average(energy_demand_dir_1) * tf_at_peak * eta * mu
     else:
         e_average_1[name] = 0
+
+
+# TODO ADDING PARAMETERS:
+#   - traffic density to rush hour (check)
+#   - percentage of cars on highway which are travelling long-distance (and therefore need to charge on highway) (check)
+
+
